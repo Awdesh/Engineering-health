faf47226221c59153957c90453c28ba384858a2d
Optimus <awdesh.sharma1@gmail.com>
2014-10-03 23:46:38 -0500

    Checking-in updated data layer.

--- TrackMyDiet/DietDataLayer.py
+++ TrackMyDiet/DietDataLayer.py
@@ -1,23 +1,27 @@
-class Diet:
-	# Intent to use dictionary is in future it can contain other fiels like person, date & time food eaten etc.
-	# All new items can be added as key.
-	# {foodName: Banana
-    #  person : test,
-    #  dateTime: 03/10/2014}
-    dietCollection = {}             
-    # below method takes care of adding food eaten inside dictionary.
-    def add_foodEaten(self, food):
-        self.dietCollection['foodName'] = food
-    # below method takes care of adding food eaten inside dictionary.
-    def delete_foodEaten(self, food):
-        del self.dietCollection['food']
-    # below method takes care of adding food eaten inside dictionary.
-    def update_foodEaten(self, food):
-        self.dietCollection['foodName'] = food
-    # below method takes care of adding food eaten inside dictionary.
-    def complete_oneDayDiet():
-    	count = Counter(dietCollection.values())
-    	return c
+# Data Layer of the applcation which populdates colleciton or call databse
+# In an ideal world this should call databse and populate it but 32 lines like Collection 
+# more. 
+# Author -: Awdesh Sharma
+# Date-: 03/10/2014
+
+# Intent to use dictionary is in future it can contain other fiels like person, date & time food eaten etc.
+# All new items can be added as key.
+# {foodId: Banana
+#  person : test,
+#  dateTime: 03/10/2014}
+dietCollection = {}             
+# below method takes care of adding food eaten inside dictionary.
+def add_foodEaten(self, food):
+    dietCollection['foodId'] = food
+# below method deletes food eaten based on foodId inside dictionary.
+def delete_foodEaten(self, foodId):
+    del dietCollection['foodId']
+# below method takes care of updating/modifying food eaten inside dictionary.
+def update_foodEaten(self, food):
+    dietCollection['foodId'] = food
+# below method will return filled dictionary back to the caller function.
+def complete_oneDayDiet():
+	return dietCollection
 
 
 
